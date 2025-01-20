0720b9c7312f8c9fa7799ef7ff6ffac7658597ef
V-Gulev <velizargulev@abv.bg>
2025-01-20 21:34:34 +0200

    WriteEveryThirdLine_05

---
+++ SoftUni/WriteEveryThirdLine_05.java
@@ -0,0 +1,33 @@
+package SoftUni;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class WriteEveryThirdLine_05 {
+    public static void main(String[] args) {
+        String inputPath = "C:\\Users\\vilis\\Developer\\JavaAdvanced\\SoftUni-Streams-Files-Directories-Exercise\\files\\Resources\\input.txt";
+        String outputPath = "C:\\Users\\vilis\\Developer\\JavaAdvanced\\SoftUni-Streams-Files-Directories-Exercise\\files\\Resources\\05.WriteEveryThirdLineOutput.txt";
+
+        try (
+                BufferedReader bufferedReader = Files.newBufferedReader(Path.of(inputPath));
+                BufferedWriter bufferedWriter = Files.newBufferedWriter(Path.of(outputPath))
+        ) {
+            String line = bufferedReader.readLine();
+            int counter = 0;
+            while (line != null) {
+                if (counter % 3 == 0) {
+                    bufferedWriter.write(line);
+                    bufferedWriter.newLine();
+                }
+                counter++;
+                line = bufferedReader.readLine();
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
