96fd2edce1c562786db795f966878c07a25a71b3
V-Gulev <velizargulev@abv.bg>
2025-01-20 20:37:49 +0200

    ExtractIntegers_04

--- SoftUni/ExtractIntegers_04.java
+++ SoftUni/ExtractIntegers_04.java
@@ -1,23 +1,23 @@
 package SoftUni;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.Scanner;
 
 public class ExtractIntegers_04 {
     public static void main(String[] args) throws FileNotFoundException {
         String path = "C:\\Users\\vilis\\Developer\\JavaAdvanced\\SoftUni-Streams-Files-Directories-Exercise\\files\\Resources\\input.txt";
+        String outPath = "C:\\Users\\vilis\\Developer\\JavaAdvanced\\SoftUni-Streams-Files-Directories-Exercise\\files\\Resources\\04.ExtractIntegersOutput.txt";
         InputStream inputStream = new FileInputStream(path);
         Scanner sc = new Scanner(inputStream);
+        PrintWriter out = new PrintWriter(new FileOutputStream(outPath));
 
         while (sc.hasNext()) {
             if (sc.hasNextInt()) {
-                System.out.println(sc.nextInt());
+               out.println(sc.nextInt());
             }
             sc.next();
         }
-
+        out.close();
 
     }
 }
