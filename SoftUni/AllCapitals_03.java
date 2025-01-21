5101f76bab6a44d00fec67191a7803025b99fd44
V-Gulev <velizargulev@abv.bg>
2025-01-21 17:13:30 +0200

    AllCapitals_03

--- SoftUni/AllCapitals_03.java
+++ SoftUni/AllCapitals_03.java
@@ -1,5 +1,7 @@
 package SoftUni;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -8,7 +10,8 @@ public class AllCapitals_03 {
     public static void main(String[] args) throws IOException {
         String inputPath = "files/Resources/04. Java-Advanced-Files-and-Streams-Exercises-Resources/Exercises Resources/input.txt";
         String upperCase = Files.readString(Path.of(inputPath)).toUpperCase();
-        System.out.println(upperCase);
-
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("files/Resources/04. Java-Advanced-Files-and-Streams-Exercises-Resources/Exercises Resources/output.txt"));
+        bufferedWriter.write(upperCase);
+        bufferedWriter.close();
     }
 }
